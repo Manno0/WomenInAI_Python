d8fe3df77998e2180e2fd36e8fa1b695f08b4f64
Manno0 <Jeladzemary@gmail.com>
2025-09-17 23:33:02 +0400

    Homework 1 - Final Commit

--- Homework_1/main.py
+++ Homework_1/main.py
@@ -2,11 +2,10 @@ import random
 x= random.randint(1,20)
 y= random.randint(1,10)
 print(f"{x} * {y} = ?")
-
 try:
     user_input = int(input("Your answer: "))
     if user_input == (x*y):
-        print("You are correct")
+        print("You are correct!")
     else:
         print("Correct answer : {x*y}")
 except ValueError:
