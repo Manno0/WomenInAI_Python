ade286eaa28a90d1e12ee4eb156542fe7020359d
Manno0 <Jeladzemary@gmail.com>
2025-09-17 23:09:40 +0400

    Homework 1 - Final Commit

---
+++ Homework_1/main.py
@@ -0,0 +1,14 @@
+import random
+x= random.randint(1,20)
+print(x)
+y= random.randint(1,10)
+print(f"{x} * {y} = ?")
+
+try:
+    user_input = int(input("Your answer: "))
+    if user_input == (x*y):
+        print("You are correct")
+    else:
+        print("Correct answer : {x*y}")
+except ValueError:
+    print("Please insert an integer")
